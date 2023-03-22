9c52e10c2851ad87b75b3a90c05ecbdecfd97f73
AikoFr <aikootv@gmail.com>
2023-03-22 11:59:12 +0100

    Premier bouton fonctionnel Shop

--- script.js
+++ script.js
@@ -3,60 +3,106 @@ const tabs = document.querySelectorAll('.tab')
 // Je stock tout mon content
 const tabContents = document.querySelectorAll(".tab-content")
 
+    // Récupère le nombre de clics sauvegardé localement (s'il existe)
+    let clicks = localStorage.getItem("clicks");
+    // Récupère les stats par click (s'il existe)
+    let perClicks = localStorage.getItem("perclicks");
+    let perClicksPrice = localStorage.getItem("perclicksprice");
 
-function tabsAnimation(num){
-    //On retirer la class de la tab a retirer (tab + content)
-    tabs.forEach((tab, index) => {
-       tab.classList.remove("active-tab")
-        tabContents[index].classList.remove("active-tab-content")
-    })
-
-    // On ajoute la class pour montrer tab + content
-    tabs[num].classList.add("active-tab")
-    tabContents[num].classList.add("active-tab-content")
-}
 
 
 // STOCKAGE DES DONNEES :
-
-//Stock dans une variable - Une très légére opti et code plus lisible
-const CLICKS_KEY = "clicks";
-
 // Vérifie si le navigateur prend en charge le stockage local
 if (typeof(Storage) !== "undefined") {
-    // Récupère le nombre de clics sauvegardé localement (s'il existe)
-    let clicks = localStorage.getItem(CLICKS_KEY);
+
     if (clicks !== null) {
       // Met à jour le nombre de clics avec la valeur stockée localement
       document.getElementById("clicks").textContent = clicks;
+    } 
+
+    //Initialise la donnée perClicks
+    if (perClicks !== null) {
+      // Met à jour le nombre de perclicks avec la valeur stockée localement
+      document.getElementById("perClicks").textContent = perClicks;
+      console.log("perClicks trouvé !")
+    } else {
+      // Initialise le nombre de perclicks à 1 s'il n'existe pas encore
+      localStorage.setItem("perclicks", 1);
+      console.log("perClicks pas trouvé, dépars a 1 !")
     }
-  } else {
-    // Le navigateur ne prend pas en charge le stockage local
-    document.getElementById("clicks").textContent = "Désolé, votre navigateur ne prend pas en charge le stockage local.";
-  }
+
+    //Initialise la donnée perClicksPrice
+    if (perClicksPrice !== null) {
+      // Met à jour le nombre de perClicksPrice avec la valeur stockée localement
+      document.getElementById("perClicksPrice").textContent = perClicksPrice;
+      console.log("perClicksPrice trouvé !")
+    } else {
+      // Initialise le nombre de perclicksprice à 25 s'il n'existe pas encore
+      localStorage.setItem("perclicksprice", 25);
+      console.log("perClicksPrice pas trouvé, dépars a 25 !")
+    }
+
+} else {
+  // Le navigateur ne prend pas en charge le stockage local
+  document.getElementById("clicks").textContent = "Désolé, votre navigateur ne prend pas en charge le stockage local.";
+}
+
+
+
+// Fonction appelée lorsque le joueur clique sur le bouton
+function onClick() {
+  // Récupère le nombre de clics actuel + boost
+  let clicks = parseInt(document.getElementById("clicks").innerHTML);
+  let perClicks = parseInt(document.getElementById("perClicks").textContent);
+  
+  // Incrémente le nombre de clics
+  clicks += perClicks;  // Affiche le nouveau nombre de clics
+
+  document.getElementById("clicks").textContent = clicks;
+  // Stocke le nouveau nombre de clics localement
+  localStorage.setItem("clicks", clicks);
+}
   
 
-  // Fonction appelée lorsque le joueur clique sur le bouton
-  function onClick() {
-    // Récupère le nombre de clics actuel
-    let clicks = parseInt(document.getElementById("clicks").innerHTML);
-    // Incrémente le nombre de clics
-    clicks++;
-    // Affiche le nouveau nombre de clics
+function cpcUp(){
+  let clicks = parseInt(document.getElementById("clicks").textContent);
+
+  if (clicks >= perClicksPrice) {
+    let perClicks = parseInt(document.getElementById("perClicks").textContent);
+    perClicks++;
+    document.getElementById("perClicks").textContent = perClicks;
+    clicks -= perClicksPrice;
+    perClicksPrice = perClicksPrice * 1.6;
     document.getElementById("clicks").textContent = clicks;
-    // Stocke le nouveau nombre de clics localement
-    localStorage.setItem(CLICKS_KEY, clicks);
-  
-    // console.log(clicks)
+    document.getElementById("perClicksPrice").textContent = perClicksPrice;
+    localStorage.setItem("clicks", clicks);
+    localStorage.setItem("perclicks", perClicks);
+    localStorage.setItem("perclicksprice", perClicksPrice);
+
+  } else {
+    alert("Not Enough Cookies!");
+
   }
+}
+
+
+
+
+
+function tabsAnimation(num){
+  //On retirer la class de la tab a retirer (tab + content)
+  tabs.forEach((tab, index) => {
+    tab.classList.remove("active-tab")
+      tabContents[index].classList.remove("active-tab-content")
+  })
+
+  // On ajoute la class pour montrer tab + content
+  tabs[num].classList.add("active-tab")
+  tabContents[num].classList.add("active-tab-content")
+}
   
-      // Plus opti de passer par un EventListener ? 
-    // document.getElementById("button").addEventListener("click", onClick);
-
-
 
 const modeSombreToggle = document.querySelector('.checkbox');
-
 modeSombreToggle.addEventListener('click', () => {
     document.body.classList.toggle('body-sombre');
 });
