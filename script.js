ec80716dd1baba49c0bb14caeb64c38b1f0f23a0
AikoFr <aikootv@gmail.com>
2023-03-23 16:35:04 +0100

    Ajout de la fonctionnalité cookie a la seconde

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const tabs = document.querySelectorAll('.tab')
 // Je stock tout mon content
 const tabContents = document.querySelectorAll(".tab-content")
 
-    // Récupère le nombre de clics sauvegardé localement (s'il existe)
-    let clicks = localStorage.getItem("clicks");
+// Récupère le nombre de clics sauvegardé localement (s'il existe)
+let clicks = localStorage.getItem("clicks");
 
-    // Récupère les stats par click (s'il existe)
-    let perClicks = localStorage.getItem("perclicks");
-    let perClicksPrice = localStorage.getItem("perclicksprice");
+// Récupère les stats par click (s'il existe)
+let perClicks = localStorage.getItem("perclicks");
+let perClicksPrice = localStorage.getItem("perclicksprice");
 
-    // Récupère les stats par click (s'il existe)
-    let perSeconds = localStorage.getItem("perseconds");
-    let perSecondsPrice = localStorage.getItem("persecondsprice");
+// Récupère les stats par click (s'il existe)
+let perSeconds = localStorage.getItem("perseconds");
+let perSecondsPrice = localStorage.getItem("persecondsprice");
+let giveCook = false;
 
+if (perSeconds >= 1){
+  launchAutomatiqueCookie()
+}
 
 // STOCKAGE DES DONNEES :
 // Vérifie si le navigateur prend en charge le stockage local
@@ -93,21 +97,21 @@ function onClick() {
   localStorage.setItem("clicks", clicks);
 }
 
-// function launchAutomatiqueCookie(bool){
-//   let giveCook = bool;
-//   console.log(giveCook)
+function launchAutomatiqueCookie(){
+  // console.log(giveCook)
 
-//   if (giveCook == true){
-//     setInterval(function(){
-//       let perSeconds = localStorage.getItem("perseconds");
-//       let clicks = parseInt(document.getElementById("clicks").textContent);
-//         clicks += +perSeconds;
+  if (giveCook === false){
+    setInterval(function(){
+      let perSeconds = localStorage.getItem("perseconds");
+      let clicks = parseInt(document.getElementById("clicks").textContent);
+        clicks += +perSeconds;
 
-//       document.getElementById("clicks").textContent = clicks;
-//       localStorage.setItem("clicks", clicks);
-//     }, 1000)
-//   }
-// }
+      document.getElementById("clicks").textContent = clicks;
+      localStorage.setItem("clicks", clicks);
+    }, 1000)
+    giveCook = true
+  }
+}
 
 
 // ICI CA MARCHE MAIS CEST DE LA MERDE
@@ -123,20 +127,35 @@ function onClick() {
 
 function cpcUp(){
   let clicks = parseInt(document.getElementById("clicks").textContent);
-
+  
+  //Ici on vérifie si le joueurs possède une quantité de cookie (clicks) supérieur ou égal au prix d'achat de l'amélioration(perClicksPrice).
   if (clicks >= perClicksPrice) {
+    //Si oui :
+    //On récup la valeur actuelle de l'amélioration(perClicks).
     let perClicks = parseInt(document.getElementById("perClicks").textContent);
-    perClicks++;
-    document.getElementById("perClicks").textContent = perClicks;
-    clicks -= perClicksPrice;
-    perClicksPrice = perClicksPrice * 1.2;
-    perClicksPrice = Math.round(perClicksPrice);
+
+      //on ajoute +1 a notre amélioration(perClicks)
+      perClicks++;
+      //On met a jour sur l'HTML la nouvelle valeur de l'amélioration(onglet du milieu)
+      document.getElementById("perClicks").textContent = perClicks;
+
+      //On déduit le prix d'achat du nombre de cookie du joueurs.
+      clicks -= perClicksPrice;
+
+      //On augmente le prix d'achat par 1.2% et on arrondit
+      perClicksPrice = perClicksPrice * 1.2;
+      perClicksPrice = Math.round(perClicksPrice);
+
+    //On met a jours l'HTML avec les nouvelles valeurs (clicks = nombre de cookie / perClicksPrice = prix de l'amélio.)
     document.getElementById("clicks").textContent = clicks;
     document.getElementById("perClicksPrice").textContent = perClicksPrice;
+
+    //Puis on met a jours les données du localStorage avec les nouvelles valeurs.
     localStorage.setItem("clicks", clicks);
     localStorage.setItem("perclicks", perClicks);
     localStorage.setItem("perclicksprice", perClicksPrice);
 
+    //Si le joueurs n'a pas assez de cookie alors on affiche un message d'erreur
   } else {
     alert("Not Enough Cookies!");
 
@@ -161,8 +180,8 @@ function secUp(){
   localStorage.setItem("clicks", clicks);
   localStorage.setItem("persecondsprice", perSecondsPrice)
   localStorage.setItem("perseconds", perSeconds);
-  // launchAutomatiqueCookie(true)
-  } else {
+  launchAutomatiqueCookie()
+} else {
     alert("Not Enough Cookies!");
   }
 }
