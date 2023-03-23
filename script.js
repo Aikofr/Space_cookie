f5ed2f3e1cef3deb1046511554db14ed7c2dda71
AikoFr <aikootv@gmail.com>
2023-03-23 16:09:01 +0100

    Initial commit

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const tabContents = document.querySelectorAll(".tab-content")
 
     // Récupère le nombre de clics sauvegardé localement (s'il existe)
     let clicks = localStorage.getItem("clicks");
+
     // Récupère les stats par click (s'il existe)
     let perClicks = localStorage.getItem("perclicks");
     let perClicksPrice = localStorage.getItem("perclicksprice");
+
+    // Récupère les stats par click (s'il existe)
+    let perSeconds = localStorage.getItem("perseconds");
+    let perSecondsPrice = localStorage.getItem("persecondsprice");
 
 
 // STOCKAGE DES DONNEES :
@@ -41,6 +46,32 @@ if (typeof(Storage) !== "undefined") {
       console.log("perClicksPrice pas trouvé, dépars a 25 !")
     }
 
+    //Initialise la donnée perSeconds
+    if (perSeconds !== null) {
+      // Met à jour le nombre de perClicksPrice avec la valeur stockée localement
+      document.getElementById("perSeconds").textContent = perSeconds;
+      console.log("perSeconds trouvé !")
+    } else {
+      // Initialise le nombre de perSeconds à 0 s'il n'existe pas encore
+      localStorage.setItem("perseconds", 0);
+      console.log("perSeconds pas trouvé, dépars a 0 !")
+    }
+
+    //Initialise la donnée perSeconds
+    if (perSecondsPrice !== null) {
+      // Met à jour le nombre de perSecondsPrice avec la valeur stockée localement
+      document.getElementById("perSecondsPrice").textContent = perSecondsPrice;
+      console.log("perSecondsPrice trouvé !")
+    } else {
+      // Initialise le nombre de perSecondsPrice à 30 s'il n'existe pas encore
+      localStorage.setItem("persecondsprice", 30);
+      console.log("perSecondsPrice pas trouvé, dépars a 30 !")
+    }
+
+    // if (perSeconds >= 1){
+    //   launchAutomatiqueCookie(true)
+    // }
+
 } else {
   // Le navigateur ne prend pas en charge le stockage local
   document.getElementById("clicks").textContent = "Désolé, votre navigateur ne prend pas en charge le stockage local.";
@@ -61,6 +92,33 @@ function onClick() {
   // Stocke le nouveau nombre de clics localement
   localStorage.setItem("clicks", clicks);
 }
+
+// function launchAutomatiqueCookie(bool){
+//   let giveCook = bool;
+//   console.log(giveCook)
+
+//   if (giveCook == true){
+//     setInterval(function(){
+//       let perSeconds = localStorage.getItem("perseconds");
+//       let clicks = parseInt(document.getElementById("clicks").textContent);
+//         clicks += +perSeconds;
+
+//       document.getElementById("clicks").textContent = clicks;
+//       localStorage.setItem("clicks", clicks);
+//     }, 1000)
+//   }
+// }
+
+
+// ICI CA MARCHE MAIS CEST DE LA MERDE
+// setInterval(function() {
+//   let clicks = parseInt(document.getElementById("clicks").textContent);
+//   let perSeconds = parseInt(document.getElementById("perSeconds").textContent);
+//   clicks += perSeconds;
+//   document.getElementById("clicks").textContent = clicks;
+//   localStorage.setItem("clicks", clicks);
+//   console.log("JE NE SUIS PAS OPTI DU TOUT")
+// }, 1500);
   
 
 function cpcUp(){
@@ -71,7 +129,8 @@ function cpcUp(){
     perClicks++;
     document.getElementById("perClicks").textContent = perClicks;
     clicks -= perClicksPrice;
-    perClicksPrice = Math.round(perClicksPrice * 1.2);
+    perClicksPrice = perClicksPrice * 1.2;
+    perClicksPrice = Math.round(perClicksPrice);
     document.getElementById("clicks").textContent = clicks;
     document.getElementById("perClicksPrice").textContent = perClicksPrice;
     localStorage.setItem("clicks", clicks);
@@ -85,7 +144,28 @@ function cpcUp(){
 }
 
 
+function secUp(){
+  let clicks = parseInt(document.getElementById("clicks").textContent);
 
+  if(clicks >= perSecondsPrice) {
+    let perSeconds = parseInt(document.getElementById("perSeconds").textContent);
+    perSeconds++;
+    document.getElementById('perSeconds').textContent = perSeconds;
+
+    clicks -= perSecondsPrice;
+    perSecondsPrice = perSecondsPrice * 2;
+    perSecondsPrice = Math.round(perSecondsPrice)
+  document.getElementById('perSecondsPrice').textContent = perSecondsPrice;
+  document.getElementById("clicks").textContent = clicks;
+
+  localStorage.setItem("clicks", clicks);
+  localStorage.setItem("persecondsprice", perSecondsPrice)
+  localStorage.setItem("perseconds", perSeconds);
+  // launchAutomatiqueCookie(true)
+  } else {
+    alert("Not Enough Cookies!");
+  }
+}
 
 
 function tabsAnimation(num){
